2499af48c0f587c033466b09b195c14344568712
JasonGrant <contactjasongrant@gmail.com>
2018-10-22 20:39:54 -0400

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -9,13 +9,8 @@ import Layout from "./mixins/layout";
 import "./registerServiceWorker";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
-import VueAnalytics from "vue-analytics";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueAnalytics, {
-  id: "UA-127940131-1"
-});
 
 Vue.mixin(Layout);
 
